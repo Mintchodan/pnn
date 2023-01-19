3572d8b01bc0ade7e7e092d56b560792c54ee15e
Mintchodan <z78z@naver.com>
2023-01-19 22:32:10 +0900

    +랜딩페이지, 디테일 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./Component/LandingPage";
+import Detail from "./Component/Detail";
 
 function App() {
-  const shoes = [
-    { id: 1, title: "운동화", price: "10000" },
-
-    { id: 2, title: "구두", price: "15000" },
-
-    { id: 3, title: "군화", price: "5000" },
-  ];
-
   return (
-    <div className="App">
-      <header className="App-header">ReactBlog</header>
-
-      <div className="App-container">
-        <div className="container-row">
-          {shoes.map((list, i) => (
-            <Card list={list} key={list.id}></Card>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Card(props) {
-  return (
-    <div className="col-md-4">
-      <img
-        src={
-          "http://codingapple1.github.io/shop/shoes" + props.list.id + ".jpg"
-        }
-        width="80%"
-        className="styleImage"
-      ></img>
-      <hr></hr>
-      <h3>{props.list.title}</h3>
-      <p>{props.list.price}</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={LandingPage} />
+        <Route exact path="/detail" element={Detail} />
+        <Route
+          exact
+          path="/event/one"
+          element={
+            <div>
+              <header className="App-header">ReactBlog</header>
+              <h2>오늘의이벤트</h2>
+              <p>첫 주문시 양배추즙 서비스</p>
+            </div>
+          }
+        />
+        <Route
+          exact
+          path="/event/two"
+          element={
+            <div>
+              <header className="App-header">ReactBlog</header>
+              <h2>오늘의이벤트</h2>
+              <p>생일기념 쿠폰받기</p>
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
